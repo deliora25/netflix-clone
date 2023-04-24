0dbfed9fb85618e506e2d128aed65d898187ff71
Gabriel Gonzales <gabrielgnzls94@gmail.com>
2023-04-24 22:21:27 +0800

    Initial Commit

--- components/Modals.tsx
+++ components/Modals.tsx
@@ -1,10 +1,14 @@
 import { XCircleIcon } from "@heroicons/react/24/outline";
+import { SpeakerXMarkIcon, SpeakerWaveIcon } from "@heroicons/react/24/outline";
 import { modalState, movieState } from "../atoms/modalAtom";
 import Modal from "@mui/material/Modal";
 import { useRecoilState } from "recoil";
 import { useEffect, useState } from "react";
 import { Element, Genre } from "@/typings";
 import ReactPlayer from "react-player/lazy";
+import { FaPlay } from "react-icons/fa";
+import { AiOutlinePlus } from "react-icons/ai";
+import { FiThumbsUp } from "react-icons/fi";
 
 function Modals() {
   const [showModal, setShowModal] = useRecoilState(modalState);
@@ -53,10 +57,17 @@ function Modals() {
   console.log(trailer);
 
   return (
-    <Modal open={showModal} onClose={handleClose}>
+    <Modal
+      open={showModal}
+      onClose={handleClose}
+      className="fixex !top-7 left-0 right-0 z-50 mx-auto w-full max-w-5xl overflow-hidden overflow-y-scroll rounded-md scrollbar-hide"
+    >
       <>
-        <button className="modalButton absolute right-5 top-5 !z-40 h-9 w-9 border-none bg-[#181818] hover:bg-[#181818]">
-          <XCircleIcon className="h-6 w-6" onClick={handleClose} />
+        <button
+          className="modalButton absolute right-5 top-5 !z-40 h-9 w-9 border-none bg-[#181818] hover:bg-[#181818]"
+          onClick={handleClose}
+        >
+          <XCircleIcon className="h-6 w-6" />
         </button>
         <div className="relative pt-[56.25%] ">
           <ReactPlayer
@@ -67,8 +78,63 @@ function Modals() {
             muted={muted}
             playing
           />
-          <div>
-            <div></div>
+          <div className="absolute bottom-10 flex w-full items-center justify-between px-10">
+            <div className="flex space-x-2">
+              <button className="flex items-center gap-x-2 rounded bg-white px-8 text-xl font-bold text-black transition hover:bg-[#e6e6e6]">
+                <FaPlay className="h-7 w-7 text-black" />
+                Play
+              </button>
+              <button className="modalButton">
+                <AiOutlinePlus className="h-7 w-7" />
+              </button>
+              <button className="modalButton">
+                <FiThumbsUp className="h-7 w-7" />
+              </button>
+              <div>
+                <button
+                  className="modalButton"
+                  onClick={() => setMuted(!muted)}
+                >
+                  {muted ? (
+                    <SpeakerXMarkIcon className="h-6 w-6" />
+                  ) : (
+                    <SpeakerWaveIcon className="h-6 w-6" />
+                  )}
+                </button>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="flex space-x-16 rounded-b-md bg-[#181818] px-10 py-8 ">
+          <div className="space-y-6 text-lg">
+            <div className="flex items-center space-x-2 text-sm">
+              <p className="font-semibold text-green-400 ">
+                {movie!.vote_average * 10}% Match
+              </p>
+              <p className="font-light ">
+                {movie?.release_date || movie?.first_air_date}
+              </p>
+              <div className="flex h-4 items-center justify-center rounded border border-white/40 px-1.5 text-xs">
+                HD
+              </div>
+            </div>
+            <div className="flex flex-col gap-x-10 gap-y-4 font-light md:flex-row">
+              <p className="w-5/6 ">{movie?.overview}</p>
+              <div className="flex flex-col space-y-3 text-sm">
+                <div>
+                  <span className="text-[gray]">Genres: </span>
+                  {genres?.map((genre) => genre.name).join(", ")}
+                </div>
+                <div>
+                  <span className="text-[gray] ">Original language: </span>
+                  {movie?.original_language}
+                </div>
+                <div>
+                  <span className="text-[gray]">Total view: </span>
+                  {movie?.vote_count}
+                </div>
+              </div>
+            </div>
           </div>
         </div>
       </>
